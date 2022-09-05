8a3ccb9cf447f3c9c64a26831bed8718e980e5c3
Jon Sterling <jon@jonmsterling.com>
2022-09-05 16:55:30 +0200

    Deploy

--- macros/guitart.rkt
+++ macros/guitart.rkt
@@ -1,5 +1,6 @@
 #lang at-exp racket
 
+(require racket/format)
 (require "macro-kit.rkt")
 
 (define-global (Sub x)
@@ -10,8 +11,14 @@
 (define-global (cate x)
   @raw{\mathsf{@x}})
 
-(define-global (Cat)
-  (cate "Cat"))
+
+(define-syntax-rule (define-cats name ...)
+  (begin
+    (define-global (name)
+      (cate (id->string name)))
+    ...))
+
+(define-cats Cat Ab Set Prof Grp Elts)
 
 (define-global (xto)
   "\\xrightarrow")
@@ -22,7 +29,40 @@
 (define-global (To)
   "\\Rightarrow")
 
+(define-global (tdown)
+  "\\mathop{\\triangledown}")
+(define-global (btdown)
+  "\\mathop{\\blacktriangledown}")
+
+(define-global (mop x)
+  @raw{\text{@x}})
+
+(define-syntax-rule (declare-mop name ...)
+  (begin
+    (define-global (name)
+      (mop (id->string name)))
+    ...))
+
+(declare-mop colim op Ran Lan)
+
+
+(define-global (firstblank)
+  "-")
+
+(define-global (sndblank)
+  "=")
+
 (define-global (esq x y z w)
-  ;;; (x "\xrightarrow[" y "]{" z "}" w))
-  @x "\\xrightarrow[" @y "]{" @z "}" @w)
+  @raw{@x \xrightarrow[@y]{@z}@w})
+
+(define-global (smat x)
+  @raw{\left( \begin{smallmatrix} @x \end{smallmatrix} \right)})
+
+(define-global (nbsmat x)
+  @raw{\begin{smallmatrix} @x \end{smallmatrix}})
+
+(define-global (tick)
+  @raw{'})
+
+
 (publish-macro-library 'guitart)
